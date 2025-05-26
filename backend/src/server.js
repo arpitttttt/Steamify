156fc301051a0ae72c9bc6994f6c8a7889714d15
Arpit Singh <121520688+arpitttttt@users.noreply.github.com>
2025-05-27 04:37:15 +0530

    userController

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.route.js';
+import userRoutes from './routes/user.route.js';
 import {connectDB} from './lib/db.js'
 
 
@@ -10,7 +12,10 @@ const app= express()
 const PORT=process.env.PORT
 
 app.use(express.json())
+app.use(cookieParser())
+
 app.use("/api/auth",authRoutes)
+app.use("/api/users", userRoutes)
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`)
