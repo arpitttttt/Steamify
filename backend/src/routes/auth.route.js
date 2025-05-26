4e724a7197bb2ef62407e64fa1a9d84d1a4fb099
Arpit Singh <121520688+arpitttttt@users.noreply.github.com>
2025-05-26 17:44:46 +0530

    upsertStream

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,17 +1,11 @@
 import express from 'express';
+import { signup, login, logout } from '../controllers/auth.controller.js';
+
 
 const router = express.Router();
 
-router.get('/signup',(req, res)=>{
-    res.send('Signup Page')
-})
-
-router.get('/login',(req, res)=>{
-    res.send('Login Page')
-})
-
-router.get('/logout',(req, res)=>{
-    res.send('Logout Page')
-}) 
+router.post('/signup', signup)
+router.post('/login', login)
+router.post('/logout', logout) 
 
 export default router;
