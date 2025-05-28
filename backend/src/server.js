9ea93b48547c8f79f141fed7db4b48e010aa38df
Arpit Singh <121520688+arpitttttt@users.noreply.github.com>
2025-05-29 04:25:42 +0530

    FrdArrayFix

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,8 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.route.js';
 import userRoutes from './routes/user.route.js';
-import {connectDB} from './lib/db.js'
+import chatRoutes from './routes/chat.route.js';
+import {connectDB} from './lib/db.js';
 
 
 dotenv.config()
@@ -16,6 +17,7 @@ app.use(cookieParser())
 
 app.use("/api/auth",authRoutes)
 app.use("/api/users", userRoutes)
+app.use("/api/chat", chatRoutes)
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`)
