f8b397e1e5ef39f2ad18d22c3cf20c042252ab72
Arpit Singh <121520688+arpitttttt@users.noreply.github.com>
2025-07-26 05:45:23 +0530

    Finally Done

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from "path";
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.route.js';
 import userRoutes from './routes/user.route.js';
@@ -13,6 +14,9 @@ const app= express()
 
 const PORT=process.env.PORT
 
+const __dirname = path.resolve();
+
+
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true,
@@ -23,6 +27,14 @@ app.use(cookieParser())
 app.use("/api/auth",authRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/chat", chatRoutes)
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}///
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`)
